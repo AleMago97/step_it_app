220f9d884c20ce69f2a5ca0476960d30db6effc4
AleMago97 <108481952+AleMago97@users.noreply.github.com>
2022-06-30 20:19:06 +0200

    Delete lib directory

--- lib/database/entities/present.dart
+++
@@ -1,9 +0,0 @@
-import 'package:floor/floor.dart';
-
-@entity
-class Present {
-  @PrimaryKey(autoGenerate: true)
-  final int? id;
-  final String name;
-  Present(this.id, this.name);
-}//Present
--- lib/utils/formats.dart
+++
@@ -1,5 +0,0 @@
-import 'package:intl/intl.dart';
-
-class Formats {
-  static final onlyDayDateFormat = new DateFormat('yyyy-MM-dd');
-} //Formats
